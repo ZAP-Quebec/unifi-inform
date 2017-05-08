e86c94e72113e7ac6ed9800e18ed23a22904c306
Jean-Sébastien Tremblay <jeansebtr@gmail.com>
2017-05-08 11:42:35 -0400

    use provided mac address in inform payload

--- client.go
+++ client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 type Client struct {
@@ -32,7 +33,9 @@ func NewClient(m MAC, address string) *Client {
 }
 
 func (c *Client) SendInform() (InformResponse, error) {
-	p := NewPacket(c.mac, &fakeInform{}, c.key)
+	p := NewPacket(c.mac, &fakeInform{
+		m: MacAddr(c.mac),
+	}, c.key)
 	body, err := p.Marshal()
 	if err != nil {
 		return nil, err
@@ -74,13 +77,15 @@ func (c *Client) StartDiscovery() {
 	// see : https://github.com/fxkr/unifi-protocol-reverse-engineering#discover
 }
 
-type fakeInform struct{}
+type fakeInform struct {
+	m MacAddr
+}
 
 func (f fakeInform) Marshal() []byte {
 	return []byte(f.String())
 }
 
-func (fakeInform) String() string {
+func (f fakeInform) String() string {
 	return `{
     "board_rev": 31,
     "bootrom_version": "unifi-v1.5.2.206-g44e4c8bc",
@@ -98,7 +103,7 @@ func (fakeInform) String() string {
         {
             "full_duplex": true,
             "ip": "0.0.0.0",
-            "mac": "04:18:d6:e0:0f:af",
+            "mac": "` + f.m.String() + `",
             "name": "eth0",
             "netmask": "0.0.0.0",
             "num_port": 2,
@@ -120,85 +125,15 @@ func (fakeInform) String() string {
     "isolated": false,
     "last_error": "Unable to resolve (http://unifi:8080/inform)",
     "locating": false,
-    "mac": "04:18:d6:e0:0f:af",
+    "mac": "` + f.m.String() + `",
     "model": "U7P",
     "model_display": "UAP-Pro",
     "netmask": "255.255.255.0",
     "qrid": "000000",
-    "radio_table": [
-        {
-            "athstats": {
-                "ast_ath_reset": 0,
-                "ast_be_xmit": 0,
-                "ast_cst": 0,
-                "ast_deadqueue_reset": 0,
-                "ast_fullqueue_stop": 0,
-                "ast_txto": 0,
-                "cu_self_rx": 0,
-                "cu_self_tx": 0,
-                "cu_total": 0,
-                "n_rx_aggr": 0,
-                "n_rx_pkts": 85,
-                "n_tx_bawadv": 0,
-                "n_tx_bawretries": 0,
-                "n_tx_pkts": 36,
-                "n_tx_queue": 0,
-                "n_tx_retries": 0,
-                "n_tx_xretries": 0,
-                "n_txaggr_compgood": 0,
-                "n_txaggr_compretries": 0,
-                "n_txaggr_compxretry": 0,
-                "n_txaggr_prepends": 0,
-                "name": "wifi0"
-            },
-            "builtin_ant_gain": 0,
-            "builtin_antenna": true,
-            "has_dfs": true,
-            "max_txpower": 23,
-            "min_txpower": 4,
-            "name": "wifi0",
-            "nss": 2,
-            "radio": "na",
-            "scan_table": []
-        },
-        {
-            "athstats": {
-                "ast_ath_reset": 0,
-                "ast_be_xmit": 2985,
-                "ast_cst": 2,
-                "ast_deadqueue_reset": 0,
-                "ast_fullqueue_stop": 0,
-                "ast_txto": 0,
-                "cu_self_rx": 15,
-                "cu_self_tx": 0,
-                "cu_total": 18,
-                "n_rx_aggr": 0,
-                "n_rx_pkts": 19853,
-                "n_tx_bawadv": 0,
-                "n_tx_bawretries": 0,
-                "n_tx_pkts": 0,
-                "n_tx_queue": 0,
-                "n_tx_retries": 0,
-                "n_tx_xretries": 0,
-                "n_txaggr_compgood": 0,
-                "n_txaggr_compretries": 0,
-                "n_txaggr_compxretry": 0,
-                "n_txaggr_prepends": 0,
-                "name": "wifi1"
-            },
-            "builtin_ant_gain": 0,
-            "builtin_antenna": true,
-            "max_txpower": 30,
-            "min_txpower": 12,
-            "name": "wifi1",
-            "nss": 3,
-            "radio": "ng",
-            "scan_table": []
-        }
-    ],
+    "radio_table": [],
     "required_version": "2.4.4",
     "selfrun_beacon": true,
-    "serial": "0418D6E00FAF",
+    "serial": "` + strings.ToUpper(f.m.HexString()) + `",
     "spectrum_scanning": false,
     "state": 1,
     "stream_token": "",
@@ -213,63 +148,8 @@ func (fakeInform) String() string {
     "time": 1494091704,
     "uplink": "eth0",
     "uptime": 346,
-    "vap_table": [
-        {
-            "bssid": "ff:ff:ff:ff:ff:ff",
-            "ccq": 5203672,
-            "channel": 36,
-            "essid": "",
-            "id": "user",
-            "name": "ath0",
-            "num_sta": 0,
-            "radio": "na",
-            "rx_bytes": 0,
-            "rx_crypts": 0,
-            "rx_dropped": 0,
-            "rx_errors": 0,
-            "rx_frags": 0,
-            "rx_nwids": 0,
-            "rx_packets": 0,
-            "sta_table": [],
-            "state": "INIT",
-            "tx_bytes": 0,
-            "tx_dropped": 2,
-            "tx_errors": 0,
-            "tx_packets": 0,
-            "tx_power": 23,
-            "tx_retries": 0,
-            "up": false,
-            "usage": "uplink"
-        },
-        {
-            "bssid": "04:18:d6:e2:0f:af",
-            "ccq": 5203672,
-            "channel": 1,
-            "essid": "0418D6E00FAF",
-            "id": "user",
-            "name": "ath1",
-            "num_sta": 0,
-            "radio": "ng",
-            "rx_bytes": 0,
-            "rx_crypts": 0,
-            "rx_dropped": 0,
-            "rx_errors": 0,
-            "rx_frags": 0,
-            "rx_nwids": 763,
-            "rx_packets": 0,
-            "sta_table": [],
-            "state": "RUN",
-            "tx_bytes": 0,
-            "tx_dropped": 151,
-            "tx_errors": 0,
-            "tx_packets": 0,
-            "tx_power": 30,
-            "tx_retries": 0,
-            "up": true,
-            "usage": "user"
-        }
-    ],
-    "version": "3.7.39.6089-custom",
+    "vap_table": [],
+    "version": "3.7.39.6089-custom3",
     "wifi_caps": 117
 }`
 }
---
+++ mac.go
@@ -0,0 +1,21 @@
+package inform
+
+import (
+	"encoding/hex"
+	"fmt"
+)
+
+type MacAddr []byte
+
+func (m MacAddr) IsValid() bool {
+	return len(m) == 6
+}
+
+func (m MacAddr) String() string {
+	return fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x",
+		m[0], m[1], m[2], m[3], m[4], m[5])
+}
+
+func (m MacAddr) HexString() string {
+	return hex.EncodeToString(m)
+}
